dc257fb929d76d800ba912905a58defaf8110a0e
Альферович Илья <Alferovich_IA_25@student.grsu.by>
2025-09-30 09:06:35 +0300

    Добавление комментариев в A7.py

--- 2 - Лабораторная работа/A7.py
+++ 2 - Лабораторная работа/A7.py
@@ -6,7 +6,7 @@ y2 = int(input())
 result = ['NO','YES'][(x1 + y1) % 2 == (x2 + y2) % 2] # Проверка на клетки покрашены в один цвет
 
 ans = ('YES')
-if result == ans:
+if result == ans: # Если result = 'YES' то у нам надо вывести YES и цвет клетки, в другом случаи вывести No
     print(result)
     color = ['White','Black'] [(x1 + y1) % 2 == 1] # Проверка , какой цвет у клетки
     print(color)
