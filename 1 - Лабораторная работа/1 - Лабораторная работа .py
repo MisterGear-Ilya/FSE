6f21f738144dfc318f24c43a843c8b607759baa9
Альферович Илья <Alferovich_IA_25@student.grsu.by>
2025-09-30 09:43:45 +0300

    Добавил папку Лабораторной 1

---
+++ 1 - Лабораторная работа/1 - Лабораторная работа .py
@@ -0,0 +1,11 @@
+n = int(input())
+line1 = "  _~_    "
+line2 = " (o o)   "
+line3 = " / V \\   "
+line4 = "/( _ )\\  "
+line5 = " ^^ ^^   "
+print(line1 * n)
+print(line2 * n)
+print(line3 * n)
+print(line4 * n)
+print(line5 * n)
